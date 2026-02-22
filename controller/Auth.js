ee96a3918f6caee2a6961be674c008ff7b5f7c8e
PRINCE KUMAR <piwari012@gmail.com>
2026-02-22 16:01:59 +0530

    Fix cookie config for production

--- controller/Auth.js
+++ controller/Auth.js
@@ -328,8 +328,8 @@ exports.login = async (req, res) => {
     const cookieOptions = {
       expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      secure: false, 
-      sameSite: "lax",
+      secure: true, 
+      sameSite: "None", 
     };
 
 
